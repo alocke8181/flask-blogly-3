ce83def70ea2e98125e31093dd622c14587eff33
alocke8181 <alocke_8181@yahoo.com>
2023-06-06 14:41:13 -0400

    added posting functionality

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,15 +17,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 connect_db(app)
 #db.create_all()
 
-# def create_users():
-#     adams = User(first_name='Alex',last_name='Adams',image_url='https://images.generated.photos/3onKkR7UoKpfi_swCssRh9wWxrK_I9jjyc59vM930II/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92Ml8w/NjE0ODcxLmpwZw.jpg')
-#     bob = User(first_name='Bob',last_name='Roberts')
-#     carla = User(first_name='Carla',last_name='Carlyle',image_url='https://images-stylist.s3-eu-west-1.amazonaws.com/app/uploads/2019/04/01103005/tomi-adeyemi-c-elena-seibert-256x256.jpg')
-#     users = [adams,bob,carla]
-#     with app.app_context():
-#         db.session.add_all(users)
-#         db.session.commit()
-
 @app.route('/')
 def home():
     """Show home page"""
@@ -35,7 +26,7 @@ def home():
 @app.route('/users/new', methods=['GET'])
 def new_user_form():
     """Show the New User Form"""
-    return render_template('new.html')
+    return render_template('new_user.html')
 
 @app.route('/users/new', methods=['POST'])
 def create_new_user():
@@ -60,7 +51,7 @@ def show_user(user_id):
 def show_edit_user_form(user_id):
     """Show the form to edit the user"""
     user = User.query.get(user_id)
-    return render_template('edit.html',user=user)
+    return render_template('edit_user.html',user=user)
 
 @app.route('/users/<int:user_id>/edit', methods=['POST'])
 def edit_user(user_id):
@@ -84,3 +75,51 @@ def delete_user(user_id):
     db.session.commit()
 
     return redirect('/')
+
+@app.route('/users/<int:user_id>/posts/new',methods=['GET'])
+def show_new_post_form(user_id):
+    """Show the new post form"""
+    user = User.query.get(user_id)
+    return render_template('new_post.html',user=user)
+
+@app.route('/users/<int:user_id>/posts/new',methods=['POST'])
+def new_post(user_id):
+    """Create a new post"""
+    title = request.form['title']
+    content = request.form['content']
+    new_post = Post(title=title, content=content, owner_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f'/posts/{post.id}')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """Show the post"""
+    post = Post.query.get(post_id)
+    return render_template('post.html',post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET'])
+def show_edit_post(post_id):
+    """Show the edit post form"""
+    post = Post.query.get(post_id)
+    return render_template('edit_post.html',post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_post(post_id):
+    """Edit the post in the db"""
+    post = Post.query.get(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    
+    db.session.add(post)
+    db.session.commit() 
+    return redirect(f'/posts/{post.id}')
+
+@app.route('/posts/<int:post_id>/delete')
+def delete_post(post_id):
+    """Delete the post"""
+    post = Post.query.get(post_id)
+    owner_id = post.owner_id
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(f'/users/{owner_id}')
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -23,3 +24,20 @@ class User(db.Model):
     def full_name(self):
         """Returns the full name of the user"""
         return f'{self.first_name} {self.last_name}'
+
+
+class Post(db.Model):
+    """Post"""
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, nullable=False, default = datetime.datetime.now)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    owner = db.relationship('User', backref='posts')
