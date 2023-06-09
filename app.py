7cf850c8df2df10485ffe56c757bc455f60ec098
alocke8181 <alocke_8181@yahoo.com>
2023-06-09 15:33:55 -0400

    added tag functionality

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -20,8 +20,19 @@ connect_db(app)
 @app.route('/')
 def home():
     """Show home page"""
-    users = User.query.order_by(User.last_name, User.first_name).all()
-    return render_template('home.html',users=users)
+    users = User.query.order_by(User.last_name, User.first_name).limit(3).all()
+    posts = Post.query.order_by(Post.created_at).limit(3).all()
+    tags = Tag.query.order_by(Tag.name).limit(3).all()
+    return render_template('home.html',users=users, posts=posts, tags=tags)
+
+################################################################################################
+#User related routes
+
+@app.route('/users')
+def show_all_users():
+    """Show all users"""
+    users = User.query.all()
+    return render_template('user/users.html', users=users)
 
 @app.route('/users/new', methods=['GET'])
 def new_user_form():
@@ -76,18 +87,31 @@ def delete_user(user_id):
 
     return redirect('/')
 
+################################################################################################
+#Post related routes
+
+@app.route('/posts')
+def show_all_posts():
+    """Show all posts"""
+    posts = Post.query.all()
+    return render_template('post/posts.html', posts= posts)
+
 @app.route('/users/<int:user_id>/posts/new',methods=['GET'])
 def show_new_post_form(user_id):
     """Show the new post form"""
     user = User.query.get(user_id)
-    return render_template('post/new_post.html',user=user)
+    tags = Tag.query.all()
+    return render_template('post/new_post.html',user=user, tags=tags)
 
 @app.route('/users/<int:user_id>/posts/new',methods=['POST'])
 def new_post(user_id):
     """Create a new post"""
     title = request.form['title']
     content = request.form['content']
-    new_post = Post(title=title, content=content, owner_id=user_id)
+    tag_ids = [int(num) for num in request.form.getlist('tag')]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+
+    new_post = Post(title=title, content=content, owner_id=user_id, tags=tags)
     db.session.add(new_post)
     db.session.commit()
     return redirect(f'/posts/{new_post.id}')
@@ -102,7 +126,8 @@ def show_post(post_id):
 def show_edit_post(post_id):
     """Show the edit post form"""
     post = Post.query.get(post_id)
-    return render_template('post/edit_post.html',post=post)
+    tags = Tag.query.all()
+    return render_template('post/edit_post.html',post=post, tags=tags)
 
 @app.route('/posts/<int:post_id>/edit', methods=['POST'])
 def edit_post(post_id):
@@ -110,6 +135,8 @@ def edit_post(post_id):
     post = Post.query.get(post_id)
     post.title = request.form['title']
     post.content = request.form['content']
+    tag_ids = [int(num) for num in request.form.getlist('tag')]
+    post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
     
     db.session.add(post)
     db.session.commit() 
@@ -123,3 +150,57 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     return redirect(f'/users/{owner_id}')
+
+################################################################################################
+#Tag related routes
+
+@app.route('/tags')
+def show_tags():
+    """Show a list of all tags"""
+    tags = Tag.query.order_by(Tag.name).all()
+    return render_template('tag/tags.html', tags=tags)
+
+
+@app.route('/tags/new', methods=['GET'])
+def show_new_tag():
+    """Show the form to make a new tag"""
+    return render_template('tag/new_tag.html')
+
+@app.route('/tags/new', methods=['POST'])
+def add_new_tag():
+    """Add new tag to db"""
+    tag_name = request.form['tag_name']
+    new_tag = Tag(name=tag_name)
+    db.session.add(new_tag)
+    db.session.commit()
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>')
+def tag_details(tag_id):
+    """Show details about a tag"""
+    tag = Tag.query.get(tag_id)
+    return render_template('/tag/details_tag.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit', methods=['GET'])
+def show_edit_tag(tag_id):
+    """Show the edit tag page"""
+    tag = Tag.query.get(tag_id)
+    return render_template('/tag/edit_tag.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit', methods=['POST'])
+def edit_tag(tag_id):
+    tag = Tag.query.get(tag_id)
+    tag.name = request.form['new_tag_name']
+
+    db.session.add(tag)
+    db.session.commit()
+
+    return redirect(f'/tags/{tag_id}')
+
+@app.route('/tags/<int:tag_id>/delete')
+def delete_tag(tag_id):
+    tag = Tag.query.get(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+    return redirect('/tags')
+
--- models.py
+++ models.py
@@ -8,7 +8,7 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-
+################################################################################################
 class User(db.Model):
     """User"""
     __tablename__ = 'users'
@@ -25,7 +25,7 @@ class User(db.Model):
         """Returns the full name of the user"""
         return f'{self.first_name} {self.last_name}'
 
-
+################################################################################################
 class Post(db.Model):
     """Post"""
     __tablename__ = 'posts'
@@ -41,3 +41,25 @@ class Post(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     owner = db.relationship('User', backref='posts')
+
+    tags = db.relationship('Tag', secondary='post_tag', backref='posts')
+
+################################################################################################
+class Tag(db.Model):
+    """Tag"""
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    name = db.Column(db.Text, unique=True, nullable=False)
+
+################################################################################################
+class PostTag(db.Model):
+    """Relationship Table b/w Post and Tag"""
+
+    __tablename__ = 'post_tag'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True )
+
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
